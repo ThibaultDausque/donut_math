5158a5a7f91ebeabb979a2e22f8cc0ee850d8c2e
Thibault Dausque <tdausque@3E6.42angouleme.fr>
2025-10-26 13:29:46 +0100

    math theory

---
+++ donut.h
@@ -0,0 +1,9 @@
+#ifndef DONUT_H
+# define DONUT_H
+
+# include <stdlib.h>
+# include <stdio.h>
+
+int	ft_donut();
+
+#endif
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "donut.h"
+
+// .,-~:;=!*#$@
+int	main(void)
+{
+	ft_donut();
+	return 0;
+}
